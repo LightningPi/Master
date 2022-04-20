ab48db9ab57b2e1b8ae2629a68d7c50a873d25fd
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-20 15:26:55 +0800

    接收使用者回饋結果，新增到資料庫

---
+++ feedback_w.php
@@ -0,0 +1,22 @@
+﻿<?php session_start();
+date_default_timezone_set("Asia/Taipei");
+
+include_once("timezone.php");
+include_once("sql.php");
+include_once("function.php");
+
+	//使用者回饋結果，寫入資料表功能，接收GET傳送值，使用Fback()寫入資料表，顯示回饋成功
+
+	$re_type = $_GET['ty'];
+	$sr_id = $_GET['sr_id'];
+
+	Fback($re_type,$sr_id);
+	
+	echo "<center><h1>回饋成功!!</h1></center>";
+	
+	
+			echo "<meta http-equiv='refresh' content='1.5;url=feedback_next.php'/>";
+	
+	
+
+?>
