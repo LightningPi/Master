caef3aa9bb14dc4122882a67b7b1fee1a5abc2a2
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-20 14:35:39 +0800

    註冊會員，新增到資料庫

---
+++ account_w.php
@@ -0,0 +1,72 @@
+﻿<?php 
+
+include_once("timezone.php");
+include_once("function.php");
+include_once("sql.php"); 
+
+
+?>
+
+<html>
+
+<head>
+
+	<title>註冊結果</title>
+
+</head>
+
+<body>
+
+<?php 
+
+	//註冊會員，php接收頁，接收POST傳送值
+
+	$account = $_POST['account'];
+		//echo $account."<br/>";
+
+	$password = $_POST['password'];
+		//echo $password."<br/>";
+
+	$u_name = $_POST['u_name'];
+		//echo $u_name."<br/>";
+
+	$u_sex = $_POST['u_sex'];
+		//echo $u_sex."<br/>";
+	
+	$date_start = $_POST['date_start'];
+		//echo $date_start."<br/>";
+
+
+
+	//註冊會員，寫入會員資料表user_feature
+
+	$u_column = "user_feature(account,password,u_name,u_sex,date_start)";
+
+	$u_values = "('".$account."','".$password."','".$u_name."',".$u_sex.",'".$date_start."')";
+
+	
+	$account_result = Insert($u_column,$u_values,"會員");
+
+	echo "<center><h1>".$account_result."</h1></center><br/>";
+
+
+?>
+
+	<center>
+
+	<input type="button" name="back_index" value="回首頁" onclick="location.href='index.php'">	
+
+	</center>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
