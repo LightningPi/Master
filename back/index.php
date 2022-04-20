f68bad9b7999d51cfc0492191f623c01fd533095
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-20 15:44:59 +0800

    後台首頁

---
+++ back/index.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php
+include_once("../timezone.php");
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>後台首頁</title>
+</head>
+
+<body>
+
+<center>
+	
+    <input type="button" value="新增搜尋結果" onclick="location.href='sr_add.php'">
+    <input type="button" value="搜尋結果列表" onclick="location.href='search_result.php'">
+
+</center>
+
+</body>
+</html>
